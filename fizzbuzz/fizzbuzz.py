b16ca156829837c2841d35a492eca471ce486343
SekthDroid <secthndamon@gmail.com>
2015-08-19 18:36:38 +0200

    Added test case for numbers divisible by 3

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -3,4 +3,6 @@ __author__ = 'SekthDroid'
 
 class FizzBuzz(object):
     def execute(self, param):
+        if param % 3 is 0:
+            return "fizz"
         return param
--- tests/FizzBuzzTest.py
+++ tests/FizzBuzzTest.py
@@ -5,9 +5,14 @@ __author__ = 'SekthDroid'
 
 
 class FizzBuzzTest(TestCase):
-
+    
     def test_should_return_number_when_not_divisible_by_three_or_five(self):
         fizzbuzz = FizzBuzz()
         self.assertEqual(1, fizzbuzz.execute(1))
         self.assertEqual(2, fizzbuzz.execute(2))
         self.assertEqual(4, fizzbuzz.execute(4))
+
+    def test_should_return_fizz_when_number_is_divisible_by_three(self):
+        fizzbuzz = FizzBuzz()
+        self.assertEqual("fizz", fizzbuzz.execute(3))
+        self.assertEqual("fizz", fizzbuzz.execute(9))
